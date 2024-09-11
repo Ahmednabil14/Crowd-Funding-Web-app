8abb9e5ffe92c838f7ee0c2b02781623e0fda300
Omar-elasrigy <merooscar87th@gmail.com>
2024-09-11 23:40:59 +0300

    edit and delete project functionalties

--- CrowdFunding/projects/urls.py
+++ CrowdFunding/projects/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import create_project
+from .views import create_project,edit_project,delete_project
 
 urlpatterns = [
-    path('create', create_project, name='create_project')
+    path('create', create_project, name='create_project'),
+    path('edit/<int:id>', edit_project, name='edit_project'),
+    path('delete/<int:id>',delete_project,name='delete_project')
 ]
--- CrowdFunding/projects/views.py
+++ CrowdFunding/projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .form import ProjectForm
 from .models import Project
 
@@ -15,6 +15,34 @@ def create_project(request):
             Project.objects.create(user=request.user, **form.cleaned_data)
             return redirect("home")
     return render(request, 'create_project.html', context=context)
+
+def edit_project(request,id):
+    project = get_object_or_404(Project, pk=id)
+    if request.user != project.user:
+        return redirect('home')
+    context = {}
+    form = ProjectForm()
+    if request.method == "POST":
+        form = ProjectForm(request.POST, request.FILES,instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            return render(request, 'edit_project.html')
+    form = ProjectForm(instance=project)
+    context = {'form': form}
+    return render(request, 'edit_project.html', context=context)
+
+def delete_project(request,id):
+    project = get_object_or_404(Project, pk=id)
+    if request.user != project.user:
+        return redirect('home')
+    if request.method == 'POST':
+        if project.total_donations < project.total_target *0.25:
+            project.delete()
+            return redirect('home')
+    context = {'project': project}
+    return render(request, 'delete_project.html',context=context)
 
 def home(request):
     context = {}
