7737121c82fe252683fa0e55418a62f92b408f0f
Omar-elasrigy <merooscar87th@gmail.com>
2024-09-12 03:20:40 +0300

    adding show Page view and templates and added  homepage functionality

--- CrowdFunding/projects/urls.py
+++ CrowdFunding/projects/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import create_project,edit_project,delete_project
+from .views import create_project,edit_project,delete_project,show_project
 
 urlpatterns = [
     path('create', create_project, name='create_project'),
     path('edit/<int:id>', edit_project, name='edit_project'),
-    path('delete/<int:id>',delete_project,name='delete_project')
+    path('delete/<int:id>',delete_project,name='delete_project'),
+    path('show/<int:id>',show_project,name='show_project')
+
 ]
--- CrowdFunding/projects/views.py
+++ CrowdFunding/projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect,get_object_or_404
 from .form import ProjectForm
 from .models import Project
+from decimal import Decimal
+from django.contrib import messages
 
 # Create your views here.
 
@@ -19,7 +21,8 @@ def create_project(request):
 def edit_project(request,id):
     project = get_object_or_404(Project, pk=id)
     if request.user != project.user:
-        return redirect('home')
+        messages.error(request, "Permission Denied")
+        return render(request,'rejected.html')
     context = {}
     form = ProjectForm()
     if request.method == "POST":
@@ -36,16 +39,20 @@ def edit_project(request,id):
 def delete_project(request,id):
     project = get_object_or_404(Project, pk=id)
     if request.user != project.user:
-        return redirect('home')
+        messages.error(request, "Permission Denied")
+        return render(request,'rejected.html')
     if request.method == 'POST':
-        if project.total_donations < project.total_target *0.25:
+        if project.total_donations < project.total_target * Decimal(0.25):
             project.delete()
             return redirect('home')
-    context = {'project': project}
-    return render(request, 'delete_project.html',context=context)
 
 def home(request):
     context = {}
     projects = Project.objects.all()
     context["projects"] = projects
     return render(request, 'home.html', context)
+
+def show_project(request,id):
+    project = get_object_or_404(Project, pk=id)
+    context={"project" :project}
+    return render(request,'show_project.html',context=context)
